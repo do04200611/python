7554b2a32c9462b12d0d3771ebe830c90527e3c2
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-06 23:04:03 +0900

    Delete 기본기 다지기 directory

--- 기본기 다지기/반복문/A+B -3.py
+++
@@ -1,6 +0,0 @@
-num = int(input()) # a의 값을 입력 받음
-
-for i in range(a) : # 입력 받은 값 만큼 실행
-    num2, num3 = list(map(int, input().split())) # 두 개의 숫자 입력 받음
-    sum = num2+num3 # 두 값을 합쳐서 변수 sum에 저장
-    print(sum) # sum 값 출력
