ba59d1d29e13401a2623a2339ce76c82d1a6e9ca
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-06 20:48:23 +0900

    Update main.py

--- Baekjoon/readme.md/main.py
+++ Baekjoon/readme.md/main.py
@@ -1,3 +1,3 @@
-a = (input().split())
-num_list = list(map(int, input().split()))
-print(str(min(num_list))+" "+str(max(num_list)))
+a = (input().split()) # a의 값을 입력 받음
+num_list = list(map(int, input().split())) #리스트 값을 입력 받음
+print(str(min(num_list))+" "+str(max(num_list))) #리스트 최소값과 최댓값 입력받음
